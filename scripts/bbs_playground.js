eaf496a4b4bdc56b9458165e67e72dca0398d6be
mk-0929 <moeka.okubo@gmail.com>
2020-07-04 21:34:21 +0900

    エラーメッセージ出力を追加

--- scripts/bbs_playground.js
+++ scripts/bbs_playground.js
@@ -1,36 +1,63 @@
-let postedComments = [];
+let errorMessages = [];
 
 function postComment() {
+  errorMessages = [];
   // 投稿内容を取得
-  const userName = user_name.value;
-  const userComment = comment.value;
-  console.log(userName.length);
-  console.log(userComment);
-  const commentContent = {
-    'userName': userName,
-    'userComment': userComment
-  };
-  // 投稿内容を配列に追加しHTMLへ表示
-  postedComments.push(commentContent);
-  console.log(postedComments);
-  const commentsArea = document.getElementById('comments_area');
-  for (let i = 0; i < postedComments.length; i++) {
-    // 要素の作成
-    const commentContents = document.createElement('div');
-    const userNameArea = document.createElement('div');
-    const userCommentArea = document.createElement('div');
-    commentContents.setAttribute('id', `comment${i}`);
-    commentContents.classList.add('content');
+  const userName = document.getElementById('user-name').value;
+  const userComment = document.getElementById('comment').value;
+  this.validateContent('名前', userName.trim(), 20);
+  this.validateContent('ひとこと', userComment.trim(), 100);
+  if (errorMessages.length <= 0) {
+    const commentContent = {
+      'userName': userName,
+      'userComment': userComment
+    };
+    // 投稿内容を配列に追加しHTMLへ表示
+    postedComments.push(commentContent);
+    const commentsArea = document.getElementById('comments-area');
+    createPostedCommentArea(postedComments);
 
-    // 投稿内容を挿入
-    userNameArea.innerHTML = `名前: ${postedComments[i].userName}`;
-    userCommentArea.innerHTML = `ひとこと: ${postedComments[i].userComment}`;
+    for (let i = 0; i < postedComments.length; i++) {
+      // 要素の作成
+      const commentContents = document.createElement('div');
+      const userNameArea = document.createElement('div');
+      const userCommentArea = document.createElement('div');
+      commentContents.setAttribute('id', `comment${i}`);
+      commentContents.classList.add('content');
 
-    // 要素に親子関係を挿入
-    commentContents.appendChild(userNameArea);
-    commentContents.appendChild(userCommentArea);
-    commentsArea.appendChild(commentContents);
+      // 投稿内容を挿入
+      userNameArea.innerHTML = `名前: ${postedComments[i].userName}`;
+      userCommentArea.innerHTML = `ひとこと: ${postedComments[i].userComment}`;
+
+      // 要素に親子関係を挿入
+      commentContents.appendChild(userNameArea);
+      commentContents.appendChild(userCommentArea);
+      commentsArea.appendChild(commentContents);
+    }
+    const formDescription = document.getElementById('form-description');
+    formDescription.innerHTML = '投稿が完了しました！';
+  } else {
+    const formContent = document.getElementById('form-content');
+    const errorArea = document.createElement('div');
+    errorArea.classList.add('error_massages');
+
+    for (let i = 0; i < errorMessages.length; i++) {
+      errorArea.innerHTML += `${errorMessages[i]}\n`
+    }
+    formContent.appendChild(errorArea);
   }
-  const formDescription = document.getElementById('form_description');
-  formDescription.innerHTML = '投稿が完了しました！'
+}
+
+function validateContent(inputName, inputValue, maxLength) {
+  if (inputValue === '') {
+    errorMessages.push(`${inputName}が入力されていません`);
+    return;
+  }
+  if (inputValue.length > maxLength) {
+    errorMessages.push(`${inputName}は${maxLength}字以内で入力してください`);
+  }
+}
+
+function createPostedCommentArea() {
+
 }
