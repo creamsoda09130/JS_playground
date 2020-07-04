f6ea890128ec6d62ae2089bcf038ce91f0430044
mk-0929 <moeka.okubo@gmail.com>
2020-07-04 18:43:11 +0900

    ボタンを押したら表示される機能追加

--- JS_playground.js
+++ JS_playground.js
@@ -1,6 +1,17 @@
-const test = 'これが表示されていれば変数はバッチリ表示されています！'
 
-const display_element = document.getElementById('display_variable')
-// display_element.innerHTML = test
+// ただただ変数を表示する部分
+const test = 'これが表示されていれば変数はバッチリ表示されています！';
+const displayElement = document.getElementById('display_variable');
+// displayElement.innerHTML = test
 // innerHTML だと元々表示されていた文も消えちゃうから insertAdjacentHTML にする
-display_element.insertAdjacentHTML('beforeend', test)
+displayElement.insertAdjacentHTML('beforeend', test);
+
+// ボタンを押したら変数を表示する部分
+function displayText() {
+    const element = document.getElementById('onclick_display');
+    if (displayBlock.style.display === 'none') {
+      console.log('ここにいるよ！')
+      displayBlock.style.display === 'block';
+    }
+    element.innerHTML = test;
+}
