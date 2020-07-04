ee1e60e904d5f8d12f8d365d816ec2a430ff3d52
mk-0929 <moeka.okubo@gmail.com>
2020-07-04 20:42:24 +0900

    投稿内容が表示できるように修正

--- scripts/bbs_playground.js
+++ scripts/bbs_playground.js
@@ -2,10 +2,35 @@ let postedComments = [];
 
 function postComment() {
   // 投稿内容を取得
-  const userName = user_name.value
-  const userComment = comment.value
-  console.log(userName)
-  console.log(userComment)
+  const userName = user_name.value;
+  const userComment = comment.value;
+  console.log(userName.length);
+  console.log(userComment);
+  const commentContent = {
+    'userName': userName,
+    'userComment': userComment
+  };
+  // 投稿内容を配列に追加しHTMLへ表示
+  postedComments.push(commentContent);
+  console.log(postedComments);
+  const commentsArea = document.getElementById('comments_area');
+  for (let i = 0; i < postedComments.length; i++) {
+    // 要素の作成
+    const commentContents = document.createElement('div');
+    const userNameArea = document.createElement('div');
+    const userCommentArea = document.createElement('div');
+    commentContents.setAttribute('id', `comment${i}`);
+    commentContents.classList.add('content');
+
+    // 投稿内容を挿入
+    userNameArea.innerHTML = `名前: ${postedComments[i].userName}`;
+    userCommentArea.innerHTML = `ひとこと: ${postedComments[i].userComment}`;
+
+    // 要素に親子関係を挿入
+    commentContents.appendChild(userNameArea);
+    commentContents.appendChild(userCommentArea);
+    commentsArea.appendChild(commentContents);
+  }
   const formDescription = document.getElementById('form_description');
   formDescription.innerHTML = '投稿が完了しました！'
 }
