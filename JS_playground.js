6180229fc6cda9117b3bfaff78c141f61df6692a
mk-0929 <moeka.okubo@gmail.com>
2020-07-04 18:44:01 +0900

    はじめdisplay none にしてる

--- JS_playground.js
+++ JS_playground.js
@@ -7,6 +7,11 @@ const displayElement = document.getElementById('display_variable');
 displayElement.insertAdjacentHTML('beforeend', test);
 
 // ボタンを押したら変数を表示する部分
+// はじめdiv要素は要らないので削除してます
+const displayBlock = document.getElementById('onclick_display');
+if (!displayBlock.innerHTML) {
+  displayBlock.style.display = 'none';
+}
 function displayText() {
     const element = document.getElementById('onclick_display');
     if (displayBlock.style.display === 'none') {
